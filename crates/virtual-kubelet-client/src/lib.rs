ca14c797fb2be6400d36032cde9d2d950f1339a3
Tancredi Orlando <tancredi.orlando@gmail.com>
2025-02-24 14:14:24 +0100

    feat(virtual-kubelet-client(: Expose public API

--- crates/virtual-kubelet-client/src/lib.rs
+++ crates/virtual-kubelet-client/src/lib.rs
@@ -3,3 +3,16 @@
 pub mod decode;
 pub mod encode;
 pub mod types;
+
+pub const WASM_MODULE_SIZE: usize = 64_000;
+
+pub type Message = crate::types::Message<WASM_MODULE_SIZE>;
+pub type Envelope = crate::types::Envelope<Message>;
+
+pub fn decode<
+    T: for <'b> minicbor::Decode<'b, ()>
+>(
+    bytes: &[u8]
+) -> Result<T, minicbor::decode::Error> {
+    minicbor::decode(bytes)
+}
