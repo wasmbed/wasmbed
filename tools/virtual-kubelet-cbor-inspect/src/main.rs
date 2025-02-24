6e7556238fbcb5853ff4e471bb0223fcd8716811
Tancredi Orlando <tancredi.orlando@gmail.com>
2025-02-24 14:14:55 +0100

    feat(virtual-kubelet-cbor-inspect): Import

---
+++ tools/virtual-kubelet-cbor-inspect/src/main.rs
@@ -0,0 +1,38 @@
+use clap::{Parser, ValueEnum};
+use virtual_kubelet_client::{Envelope, decode};
+
+#[derive(Parser)]
+#[command()]
+struct Args {
+    #[arg(long, value_enum, help = "Input format")]
+    format: Format,
+}
+
+#[derive(Clone, ValueEnum)]
+enum Format {
+    #[clap(name = "dec")]
+    Decimal,
+    #[clap(name = "hex")]
+    Hexadecimal,
+    #[clap(name = "bin")]
+    Binary,
+}
+
+fn main() {
+    let cli = Args::parse();
+    let input = std::io::read_to_string(std::io::stdin()).unwrap();
+
+    let bytes = match &cli.format {
+        Format::Hexadecimal =>
+            hex::decode(input.trim()).unwrap(),
+        Format::Decimal => input
+            .trim()
+            .split_whitespace()
+            .map(|s| s.parse::<u8>().unwrap())
+            .collect(),
+        Format::Binary => input.trim().into(),
+    };
+
+    let decoded = decode::<Envelope>(&bytes).unwrap();
+    println!("{:#?}", decoded);
+}
