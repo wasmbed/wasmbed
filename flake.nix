e73d8b8dbaabf4f04f5c7de7e0ff03af0dc87ce4
Tancredi Orlando <tancredi.orlando@gmail.com>
2025-03-21 15:35:41 +0100

    chore(nix): Add packages and checks to Flake

--- flake.nix
+++ flake.nix
@@ -10,13 +10,40 @@
     nixpkgs,
     flake-utils,
     rust-overlay,
-  }: let
-    systems = with flake-utils.lib.system; [ x86_64-linux ];
-  in flake-utils.lib.eachSystem systems (system: let
+  }:
+  flake-utils.lib.eachDefaultSystem (system: let
+    inherit (nixpkgs) lib;
     overlays = [ (import rust-overlay) ];
     pkgs = import nixpkgs { inherit system overlays; };
     toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    rustPlatform = pkgs.makeRustPlatform {
+      cargo = toolchain;
+      rustc = toolchain;
+    };
+    listDirectories = path:
+      builtins.attrNames
+        (lib.attrsets.filterAttrs
+          (_: type: type == "directory")
+          (builtins.readDir path));
+    buildPackage = { name, doCheck }: rustPlatform.buildRustPackage {
+      inherit name doCheck;
+      src = ./.;
+      cargoLock.lockFile = ./Cargo.lock;
+      auditable = false;
+    };
+    packages =
+      map
+        (dir: "wasmbed-${dir}")
+        (listDirectories ./crates);
   in {
+    checks =
+      lib.attrsets.genAttrs
+        packages
+        (name: buildPackage { inherit name; doCheck = true; });
+    packages =
+      lib.attrsets.genAttrs
+        packages
+        (name: buildPackage { inherit name; doCheck = false; });
     devShells.default = pkgs.mkShell {
       packages = [ toolchain ];
     };
