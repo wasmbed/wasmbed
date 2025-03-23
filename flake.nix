e7aa8f1a2c7809456cbb8211b36b1a81f7fe7a26
Tancredi Orlando <tancredi.orlando@gmail.com>
2025-03-23 11:07:51 +0100

    chore(nix): Use crane instead of buildRustPackage

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs = {
@@ -10,42 +11,43 @@
     nixpkgs,
     flake-utils,
     rust-overlay,
+    crane,
   }:
   flake-utils.lib.eachDefaultSystem (system: let
     inherit (nixpkgs) lib;
+
     overlays = [ (import rust-overlay) ];
     pkgs = import nixpkgs { inherit system overlays; };
-    toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-    rustPlatform = pkgs.makeRustPlatform {
-      cargo = toolchain;
-      rustc = toolchain;
-    };
+
+    mkToolchain = p: p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    craneLib = (crane.mkLib pkgs).overrideToolchain mkToolchain;
+    src = craneLib.cleanCargoSource ./.;
+
     listDirectories = path:
       builtins.attrNames
         (lib.attrsets.filterAttrs
           (_: type: type == "directory")
           (builtins.readDir path));
-    buildPackage = { name, doCheck }: rustPlatform.buildRustPackage {
-      inherit name doCheck;
-      src = ./.;
-      cargoLock.lockFile = ./Cargo.lock;
-      auditable = false;
+    packages = listDirectories ./crates;
+
+    buildPackage = { name, doCheck }: craneLib.buildPackage {
+      inherit src doCheck;
+      inherit
+        (craneLib.crateNameFromCargoToml { cargoToml = ./crates/${name}/Cargo.toml; })
+        pname;
+      cargoExtraArgs = "-p ${name}";
     };
-    packages =
-      map
-        (dir: "wasmbed-${dir}")
-        (listDirectories ./crates);
   in {
     checks =
       lib.attrsets.genAttrs
         packages
         (name: buildPackage { inherit name; doCheck = true; });
+
     packages =
       lib.attrsets.genAttrs
         packages
         (name: buildPackage { inherit name; doCheck = false; });
-    devShells.default = pkgs.mkShell {
-      packages = [ toolchain ];
-    };
+
+    devShells.default = craneLib.devShell {};
   });
 }
