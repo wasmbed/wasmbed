393c9a7395895e5ca356c56148f43cd1d750fc90
Tancredi Orlando <tancredi.orlando@gmail.com>
2025-06-27 10:40:54 +0200

    refactor(types): Make PublicKey wrap rustls types

--- crates/wasmbed-types/src/serde.rs
+++ crates/wasmbed-types/src/serde.rs
@@ -1,25 +1,29 @@
-pub(crate) mod base64_bytes {
-    use std::borrow::Cow;
-    use serde::{Serializer, Deserialize, Deserializer};
+#[cfg(feature = "cert")]
+pub(crate) mod spki_der {
+    use rustls_pki_types::SubjectPublicKeyInfoDer;
+    use serde::{Serializer, Deserializer, Deserialize};
     use serde::de::Error;
     use base64::{engine::general_purpose::STANDARD, Engine};
 
-    pub fn serialize<S>(bytes: &[u8], serializer: S) -> Result<S::Ok, S::Error>
+    pub fn serialize<S>(
+        spki: &SubjectPublicKeyInfoDer,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let encoded = STANDARD.encode(bytes.as_ref());
+        let encoded = STANDARD.encode(spki.as_ref());
         serializer.serialize_str(&encoded)
     }
 
     pub fn deserialize<'de, D>(
         deserializer: D,
-    ) -> Result<Cow<'static, [u8]>, D::Error>
+    ) -> Result<SubjectPublicKeyInfoDer<'static>, D::Error>
     where
         D: Deserializer<'de>,
     {
         let s: &str = Deserialize::deserialize(deserializer)?;
         let decoded = STANDARD.decode(s).map_err(D::Error::custom)?;
-        Ok(Cow::Owned(decoded))
+        Ok(decoded.into())
     }
 }
